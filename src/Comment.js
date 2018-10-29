6544e1f2436aa1c773cac9bf99fe3448a0e870cc
Chaim Reisman <crreisman@gmail.com>
2018-10-29 19:40:15 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 export default class Comment extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render () {
     return (
-      
+      <div>
+        { this.props.commentText }
+      </div>
     )
   }
 }
